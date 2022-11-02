0fd27f35d0c5327c708ae553faaebf78961cd248
Emilio Vera Meza <evera@coop15abril.fin.ec>
2022-11-01 23:18:39 -0500

    carga inicial

---
+++ cmsModelo/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-177^##rb15=9-&y*eg2&t5!nw12m&&3a5@=lr+n(zm%3_xvk!@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
